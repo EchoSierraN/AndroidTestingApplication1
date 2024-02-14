4f9f040c9d868603d78cfc7639632f725971ea3e
Elias Shemsu <echosierran@gmail.com>
2024-02-14 12:19:38 +0300

    Initial commit, working RegistrationUtil tests

---
+++ app/src/main/java/com/example/androidtestingapplication1/RegistrationUtil.kt
@@ -0,0 +1,15 @@
+package com.example.androidtestingapplication1
+
+object RegistrationUtil {
+
+    private val existingUsers = listOf("Elias", "Fanthood")
+
+    fun validateRegistrationInput(
+        username: String,
+        password: String,
+        confirmPassword: String
+    ) : Boolean {
+
+        return true;
+    }
+}
---
+++ app/src/test/java/com/example/androidtestingapplication1/RegistrationUtilTest.kt
@@ -0,0 +1,82 @@
+package com.example.androidtestingapplication1
+
+import com.google.common.truth.Truth
+import org.junit.Test
+
+class RegistrationUtilTest {
+
+    @Test
+    fun `empty user name returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "", "123", "123"
+        )
+
+        Truth.assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `valid username and correctly repeated password returns true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Ermhood", "123", "123"
+        )
+
+        Truth.assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`() {
+        val resultErmhood = RegistrationUtil.validateRegistrationInput(
+            "Ermhood", "123", "123"
+        )
+
+        val resultFanthood = RegistrationUtil.validateRegistrationInput(
+            "Fanthood", "123", "123"
+        )
+
+        Truth.assertThat(resultErmhood).isTrue()
+        Truth.assertThat(resultFanthood).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`() {
+        val resultPassword =
+            RegistrationUtil.validateRegistrationInput("Paulos", "", "paulos")
+        val resultConfirmation =
+            RegistrationUtil.validateRegistrationInput("Paulos", "paulos", "")
+        val resultBoth =
+            RegistrationUtil.validateRegistrationInput("Paulos", "", "")
+
+        Truth.assertThat(resultPassword).isFalse()
+        Truth.assertThat(resultConfirmation).isFalse()
+        Truth.assertThat(resultBoth).isFalse()
+
+    }
+    @Test
+    fun `passwords don't match returns false`() {
+        val resultErmhood = RegistrationUtil.validateRegistrationInput(
+            "Ermhood", "123", "124"
+        )
+
+        Truth.assertThat(resultErmhood).isFalse()
+    }
+
+    //password contains less than two digits
+    @Test
+    fun `password of less than two digits returns false`() {
+        val resultErmhood = RegistrationUtil.validateRegistrationInput(
+            "Ermhood", "1", "1"
+        )
+
+        Truth.assertThat(resultErmhood).isFalse()
+    }
+
+    @Test
+    fun `more than two special characters in username returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Juckb^X&W0@gmail.com", "124", "124"
+        )
+
+        Truth.assertThat(result).isFalse();
+    }
+
+}
